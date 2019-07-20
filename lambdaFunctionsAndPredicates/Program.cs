dd377e43ee29c901b6bb9ea4d54896fe971c3d0f
Evgeny Vendrov <evgenyv94@gmail.com>
2019-07-20 11:34:33 +0300

    creating a student class

--- lambdaFunctionsAndPredicates/Program.cs
+++ lambdaFunctionsAndPredicates/Program.cs
@@ -10,6 +10,7 @@ namespace lambdaFunctionsAndPredicates
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ lambdaFunctionsAndPredicates/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaFunctionsAndPredicates
+{
+    class Student
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public String Id
+        {
+            get;
+            set;
+        }
+
+        public double AvgGrade
+        {
+            get;
+            set;
+        }
+
+        public List<Student> getListOfStudents()
+        {
+            return new List<Student>
+            {
+                new Student() { Name = "evgeny", Id = "123456789", AvgGrade = 81.66 },
+                new Student() { Name = "yossi", Id = "987654321", AvgGrade = 71.66 },
+                new Student() { Name = "moshe", Id = "555555555", AvgGrade = 99 },
+                new Student() { Name = "haim", Id = "666666666", AvgGrade = 23.99 },
+                new Student() { Name = "whatever", Id = "012345678", AvgGrade = 77.77 },
+            };
+
+        }
+    }
+}
