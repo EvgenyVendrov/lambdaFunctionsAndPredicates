c0539be8f109190a987b0b2a48a60693cb0b58db
Evgeny Vendrov <evgenyv94@gmail.com>
2019-07-20 12:04:29 +0300

    added a main in which i test the predicates

--- lambdaFunctionsAndPredicates/Program.cs
+++ lambdaFunctionsAndPredicates/Program.cs
@@ -10,7 +10,24 @@ namespace lambdaFunctionsAndPredicates
     {
         static void Main(string[] args)
         {
-
+            var studentList = Students.getListOfStudents();
+            //printing to console first studnet with average grade above 70
+            Console.WriteLine(studentList.Find(student=>student.AvgGrade>70));
+            //printing all the students which average grade is above 70
+            var allStudentsAbove70 = studentList.FindAll(student => student.AvgGrade > 70);
+            foreach(var student in allStudentsAbove70)
+            {
+                Console.WriteLine(student);
+            }
+            //last student contains "123" in he's id
+            Console.WriteLine(studentList.FindLast(student=>student.Name.Contains("123")));
+            //printing the indexes of the first students which name starts with a letter between 'c' and 'j'
+            var index = studentList.FindIndex(student=>student.Name.ElementAt(0)>='c'&& student.Name.ElementAt(0) <= 'j');
+            Console.WriteLine("first student whose name starts with a letter between c and j: "+studentList[index]);
+            //inserting new element to the list we got earlier, now the student which returns should be the new one   
+            studentList.Insert(0,new Students() { Name="caaa",Id="1212121",AvgGrade=33});
+            index = studentList.FindIndex(student => student.Name.ElementAt(0) >= 'c' && student.Name.ElementAt(0) <= 'j');
+            Console.WriteLine("first student whose name starts with a letter between c and j: " + studentList[index]);
         }
     }
 }
---
+++ lambdaFunctionsAndPredicates/Students.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaFunctionsAndPredicates
+{
+    class Students
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public String Id
+        {
+            get;
+            set;
+        }
+
+        public double AvgGrade
+        {
+            get;
+            set;
+        }
+
+        public static List<Students> getListOfStudents()
+        {
+            return new List<Students>
+            {
+                new Students() { Name = "evgeny", Id = "123456789", AvgGrade = 81.66 },
+                new Students() { Name = "yossi", Id = "987654321", AvgGrade = 71.66 },
+                new Students() { Name = "moshe", Id = "555555555", AvgGrade = 99 },
+                new Students() { Name = "haim", Id = "666666666", AvgGrade = 23.99 },
+                new Students() { Name = "whatever", Id = "012345678", AvgGrade = 77.77 },
+            };
+
+        }
+
+        public override string ToString() {
+            return ("student name: "+this.Name+", student id:"+ this.Id+", student's average grade:" + this.AvgGrade);
+        }
+
+    }
+}
